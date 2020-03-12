043e36c93c831ee30dd0521c5f042791cf5aaf92
Matthew Brischuk <buddymjb@hotmail.com>
2020-03-11 18:42:37 -0600

    add test with no fxnality

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -93,4 +93,10 @@ class CityListTest {
 
         assertEquals(1, cityList.countCities());
     }
+
+    @Test
+    void testAddinator5000(){
+        assertEquals(4, addinator5000(2,2));
+
+    }
 }
