0787f32abe24ec2274c23bde6396087bd788d8ed
natemik <mikhail@ualberta.ca>
2021-03-21 16:12:52 -0600

    Failing test

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -93,4 +93,13 @@ class CityListTest {
 
         assertEquals(1, cityList.countCities());
     }
+
+    @Test
+    void testGetPosition() {
+        CityList cityList = mockCityList();
+
+        City mockCity = new City("Edmonton", "Alberta");
+
+        assertEquals(0, cityList.getPosition(mockCity));
+    }
 }
