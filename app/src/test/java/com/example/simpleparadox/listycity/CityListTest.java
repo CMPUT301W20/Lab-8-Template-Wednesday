a752efff870dc0837599508d65be8775464c4300
hvtraan <hvtran@ualberta.ca>
2021-03-22 13:08:55 -0600

    Step 4

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -93,15 +93,4 @@ class CityListTest {
 
         assertEquals(1, cityList.countCities());
     }
-
-    /**
-     * Should fail
-     */
-
-    @Test
-    void failCountCities() {
-        CityList cityList = mockCityList();
-
-        assertEquals(0, cityList.countCities());
-    }
 }
