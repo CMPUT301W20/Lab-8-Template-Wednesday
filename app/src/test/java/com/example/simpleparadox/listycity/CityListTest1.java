f32074d585e5102dbd021d451022258142e2de90
Hang Ngo <thngo@ualberta.ca>
2021-03-22 14:18:00 -0600

    failed test added

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest1.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest1.java
@@ -1,4 +1,23 @@
 package com.example.simpleparadox.listycity;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class CityListTest1 {
+    private CityList mockCityList() {
+        CityList cityList = new CityList();
+        cityList.add(mockCity());
+        return cityList;
+    }
+
+    private City mockCity() {
+        return new City("Edmonton", "Alberta");
+    }
+
+    @Test
+    void testAdd() {
+        CityList cityList = mockCityList();
+        assertTrue(cityList.hasCity(new City("Regina", "Saskatchewan")));
+    }
 }
