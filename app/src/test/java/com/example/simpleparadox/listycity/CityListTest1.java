565cf3be757fd75271cb16a67744e4e346493965
Hang Ngo <thngo@ualberta.ca>
2021-03-22 14:31:07 -0600

    successful test case

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest1.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest1.java
@@ -11,7 +11,7 @@ public class CityListTest1 {
         cityList.add(mockCity());
         return cityList;
     }
-///
+//
     private City mockCity() {
         return new City("Edmonton", "Alberta");
     }
