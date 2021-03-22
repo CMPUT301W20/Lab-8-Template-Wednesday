897a88169a20bd9f8909a0afb61ba25286e64fb0
sonikrutik <56977066+sonikrutik@users.noreply.github.com>
2021-03-21 22:30:41 -0600

    Added Test Without functionality

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -93,4 +93,11 @@ class CityListTest {
 
         assertEquals(1, cityList.countCities());
     }
+
+    @Test
+    void testCitiesGreaterThan(){
+        CityList cityList = mockCityList();
+        
+        assertTrue(cityList.citiesGreaterThan());
+    }
 }
